45426d8c6e6915512304470ba81ea7fed5400bfc
Benjamin Scanlan <ben.scanlan@yahoo.com>
2023-09-26 08:41:18 -0700

    version 2

--- Star_Jam/Star_Jam/GameViewController.swift
+++ Star_Jam/Star_Jam/GameViewController.swift
@@ -4,11 +4,18 @@ import SceneKit
 struct ContentView: View {
     var body: some View {
         VStack {
-            SceneView(scene: createScene(), options: [.allowsCameraControl])
+            SceneView(scene: createScene())
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                            .onChanged({ (value) in
+                                panCamera(with: value)
+                            })
+                )
                 .edgesIgnoringSafeArea(.all)
                 .frame(width: 400, height: 400)
         }
     }
+
+    
 
     func createScene() -> SCNScene {
         // Create a new scene
@@ -17,7 +24,7 @@ struct ContentView: View {
         // Create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 20)
         scene.rootNode.addChildNode(cameraNode)
 
         // Create and add a light to the scene
@@ -33,13 +40,39 @@ struct ContentView: View {
         earthNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(2 * Double.pi), z: 0, duration: 10)))
         scene.rootNode.addChildNode(earthNode)
 
-        // Create and add a satellite model (simple colored sphere)
-        let satelliteNode = SCNNode(geometry: SCNSphere(radius: 0.2))
+        // Create and add a satellite (simple colored sphere) orbiting the Earth
+        let satelliteNode = SCNNode(geometry: SCNSphere(radius: 0.5)) // Adjust the radius for the satellite
         satelliteNode.geometry?.firstMaterial?.diffuse.contents = NSColor.red // Red sphere for satellite
         satelliteNode.position = SCNVector3(x: 6, y: 0, z: 0)
-        scene.rootNode.addChildNode(satelliteNode)
+        earthNode.addChildNode(satelliteNode)
+
+        // Add an orbit animation to the satellite
+        let orbitAction = SCNAction.rotateBy(x: 0, y: CGFloat(2 * Double.pi), z: 0, duration: 5) // Adjust duration for the orbit speed
+        let repeatAction = SCNAction.repeatForever(orbitAction)
+        satelliteNode.runAction(repeatAction)
 
         return scene
+    }
+}
+
+func panCamera(with gesture: DragGesture.Value) {
+        // Calculate the new camera position based on the gesture
+        let translation = gesture.translation
+        let anglePan = Float(translation.width) * (Float.pi / 180.0)
+        let distancePan = Float(translation.height) * 0.01
+
+        // Get the current camera node
+        if let cameraNode = sceneView.pointOfView {
+            // Update the camera's position
+            cameraNode.position = SCNVector3(
+                x: cameraNode.position.x + distancePan * sin(anglePan),
+                y: cameraNode.position.y + distancePan * cos(anglePan),
+                z: cameraNode.position.z
+            )
+        }
+
+        // Reset the gesture translation
+        gesture.translation = .zero
     }
 }
 
@@ -48,4 +81,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
