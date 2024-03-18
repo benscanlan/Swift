da881555c2ef30e1d612b996b80a81d8f2f882f6
Benjamin Scanlan <ben.scanlan@yahoo.com>
2024-03-17 21:14:00 -0700

    take picture use picture app

--- upc_barcode_scanner/upc_barcode_scanner/AppDelegate.swift
+++ upc_barcode_scanner/upc_barcode_scanner/AppDelegate.swift
@@ -1,15 +1,72 @@
-import UIKit
+import SwiftUI
+
+struct CameraView: View {
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+
+    var body: some View {
+        VStack {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+            
+            Button("Take Picture") {
+                self.isShowPhotoLibrary.toggle()
+            }
+            .padding()
+            .sheet(isPresented: $isShowPhotoLibrary) {
+                ImagePicker(sourceType: .camera) { capturedImage in
+                    self.image = capturedImage
+                }
+            }
+        }
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    let sourceType: UIImagePickerController.SourceType
+    let completionHandler: (UIImage) -> Void
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(completionHandler: completionHandler)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let completionHandler: (UIImage) -> Void
+
+        init(completionHandler: @escaping (UIImage) -> Void) {
+            self.completionHandler = completionHandler
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                completionHandler(image)
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+}
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = MainViewController()
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
-        return true
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
     }
 }
 
